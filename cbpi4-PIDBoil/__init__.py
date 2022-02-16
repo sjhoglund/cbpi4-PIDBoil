75e23af9c72514bd52fe238c9b4a7d33d3372fe7
Steven Hoglund <sjhoglund@gmail.com>
2022-02-16 12:15:59 -0800

    Update throttle values to be between -1 and 1

--- cbpi4-PIDBoil/__init__.py
+++ cbpi4-PIDBoil/__init__.py
@@ -61,8 +61,8 @@ class PIDBoil(CBPiKettleLogic):
                 
                 if (heat_percent_old != heat_percent) or (heat_percent != current_kettle_power):
                     await self.actor_set_power(self.heater, heat_percent)
-                    kit.motor2.throttle = heat_percent
-                    kit.motor4.throttle = heat_percent
+                    kit.motor2.throttle = heat_percent / 100
+                    kit.motor4.throttle = heat_percent / 100
                     heat_percent_old= heat_percent
                 await asyncio.sleep(sampleTime)
 
